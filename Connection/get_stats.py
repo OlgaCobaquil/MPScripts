85064360196967e3e47233c49b6ae1f0732c44d3
OlgaCobaquil <olga.cobaquil94@gmail.com>
2018-10-17 23:22:29 -0600

    Get variables from wav

---
+++ Connection/get_stats.py
@@ -0,0 +1,25 @@
+import sox
+#.wav path
+path = "E011 p1 M.wav"
+
+#get stats from audio file
+def get_file_ready(path):
+	print("Getting audio ready")
+	tfm = sox.Transformer()
+	return tfm.stats(path)
+
+
+#get important variables 
+def get_variables(dictionary):
+
+	print("Getting variables from audio")
+	min_level = float(dictionary.get("Min level"))
+	max_level = float(dictionary.get("Max level"))
+	rms_level = float(dictionary.get("RMS lev dB"))
+	pk_level = float(dictionary.get("Pk lev dB"))
+	rms_tr = float(dictionary.get("RMS Tr dB"))
+	crest = float(dictionary.get("Crest factor"))
+	rms_pk = float(dictionary.get("RMS Pk dB"))
+	return [[rms_tr,pk_level,crest,rms_pk,min_level,max_level,rms_level]]
+a = get_file_ready(path)
+print(get_variables(a))
